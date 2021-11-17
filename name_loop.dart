44538c7580461d95ee24dcae139ef3e4a1095f63
KariiimFouad <94301032+KariiimFouad@users.noreply.github.com>
2021-11-17 18:48:17 +0200

    Name_loop

---
+++ name_loop.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  // giving condition to reach number 100 by using for loop
+  for(int i=0 ;i<101; i++){
+    //${i} this form to put number on same line with name
+    print("${i}-karim");
+
+}
+}
